a962a70e8c8ef3020a569350da52e1eb679dcc04
aneesha <aneesha.bakharia@gmail.com>
2013-07-07 23:18:37 +1000

    Working nmf code and new function to calculate reconstruction error

--- nmf.js
+++ nmf.js
@@ -3,30 +3,47 @@
 
 function nmf()
 {
-  var maxiterations = 1000;
+  var maxiterations = 5;
   var k = 2;
-  var epsilon = 0.0000001;
+  var epsilon = 0.0001;
+  var tolerance = 0.01;
+  var m,n;
   
   var A,W,H;
   
-  A = [[1,2,3],
-       [4,5,6],
-       [7,3,9]]
+  A = [[1,1,0,0],
+       [1,1,0,0],
+       [0,0,1,1],
+       [0,0,1,1]];
        
   A_dim = numeric.dim(A)
-  n = A_dim[0]
-  m = A_dim[1]
+  m = A_dim[0]
+  n = A_dim[1]
   
   W = numeric.random([m,k]); // initialize W as random matrix
   H = numeric.random([k,n]); // initialize H as random matrix
-  
-  
+    
   for (i=0; i<maxiterations; i++)
   {
     //H = H .* (W'A) ./ (W'WH + epsilon);
-    H = numeric.div(numeric.mul(H,(numeric.dot(numeric.transpose(W),A)),(numeric.dot(numeric.dot(numeric.transpose(W),W),H)) + epsilon));
+    H = numeric.div(numeric.mul(H,(numeric.dot(numeric.transpose(W),A))),(numeric.add(numeric.dot(numeric.dot(numeric.transpose(W),W),H),epsilon)));
     //W = W .* (AH' ) ./ (WHH' + epsilon);
-    W = numeric.div(numeric.mul(W,numeric.dot(A,numeric.transpose(H))),(numeric.dot(numeric.dot(W,H),numeric.transpose(H)) + epsilon));
+    W = numeric.div(numeric.mul(W,numeric.dot(A,numeric.transpose(H))),(numeric.add(numeric.dot(numeric.dot(W,H),numeric.transpose(H)),epsilon)));
   }
   
+  A_reconstructed = numeric.dot(W,H);
+  
+  //alert(numeric.prettyPrint(W));
+  
+  //alert(numeric.prettyPrint(H));
+  
+  alert(numeric.prettyPrint(A_reconstructed));
+  
+  alert(calculate_reconstructionError(A, A_reconstructed))
+  
+}
+
+function calculate_reconstructionError(A, A_reconstructed)
+{
+  return numeric.norm2(numeric.sub(A,A_reconstructed));
 }
