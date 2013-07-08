adf378e7f4209ee789b3f444f1c8fb3e3c561f25
aneesha <aneesha.bakharia@gmail.com>
2013-07-08 23:21:40 +1000

    Restructure code

--- nmf.js
+++ nmf.js
@@ -1,20 +1,18 @@
-// Javascript implementation of Non-negative Matrix Factorisation
-// using the Multiplicative Update Algorithm
+"use strict";
 
-function nmf()
+// Javascript implementation of Non-negative Matrix Factorisation Algorithm
+// based on the Multiplicative Update Algorithm
+
+var nmf = (typeof exports === "undefined")?(function nmf() {}):(exports);
+if(typeof global !== "undefined") { global.nmf = nmf; }
+
+nmf.version = "0.8";
+nmf.epsilon = 0.0001;
+
+nmf.mu = function mu(A, k, maxiterations, tolerance)
 {
-  var maxiterations = 5;
-  var k = 2;
-  var epsilon = 0.0001;
-  var tolerance = 0.01;
+  var W,H;
   var m,n;
-  
-  var A,W,H;
-  
-  A = [[1,1,0,0],
-       [1,1,0,0],
-       [0,0,1,1],
-       [0,0,1,1]];
        
   A_dim = numeric.dim(A)
   m = A_dim[0]
@@ -26,13 +24,42 @@ function nmf()
   for (i=0; i<maxiterations; i++)
   {
     //H = H .* (W'A) ./ (W'WH + epsilon);
-    H = numeric.div(numeric.mul(H,(numeric.dot(numeric.transpose(W),A))),(numeric.add(numeric.dot(numeric.dot(numeric.transpose(W),W),H),epsilon)));
+    H = numeric.div(numeric.mul(H,(numeric.dot(numeric.transpose(W),A))),(numeric.add(numeric.dot(numeric.dot(numeric.transpose(W),W),H),nmf.epsilon)));
     //W = W .* (AH' ) ./ (WHH' + epsilon);
-    W = numeric.div(numeric.mul(W,numeric.dot(A,numeric.transpose(H))),(numeric.add(numeric.dot(numeric.dot(W,H),numeric.transpose(H)),epsilon)));
+    W = numeric.div(numeric.mul(W,numeric.dot(A,numeric.transpose(H))),(numeric.add(numeric.dot(numeric.dot(W,H),numeric.transpose(H)),nmf.epsilon)));
+  
+    A_reconstructed = numeric.dot(W,H);
+    
+    if (nmf.calculate_reconstructionError <= tolerance)
+    {
+      break;
+    }
   }
   
-  A_reconstructed = numeric.dot(W,H);
+  return {W:W,H:H}
+}
+
+nmf.calculate_reconstructionError = function calculate_reconstructionError(A, A_reconstructed)
+{
+  return numeric.norm2(numeric.sub(A,A_reconstructed));
+}
+
+/*
+
+  var maxiterations = 5;
+  var k = 2;
+  var epsilon = 0.0001;
+  var tolerance = 0.01;
+  var m,n;
   
+  var A,W,H;
+  
+    A = [[1,1,0,0],
+       [1,1,0,0],
+       [0,0,1,1],
+       [0,0,1,1]];
+       
+         
   //alert(numeric.prettyPrint(W));
   
   //alert(numeric.prettyPrint(H));
@@ -40,10 +67,4 @@ function nmf()
   alert(numeric.prettyPrint(A_reconstructed));
   
   alert(calculate_reconstructionError(A, A_reconstructed))
-  
-}
-
-function calculate_reconstructionError(A, A_reconstructed)
-{
-  return numeric.norm2(numeric.sub(A,A_reconstructed));
-}
+*/
